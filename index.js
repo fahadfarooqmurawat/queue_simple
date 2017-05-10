9db7d3110eae4296cbc0e49c33f58b91605fa859
fahadfarooqmurwat <fahadfarooqmurwat@gmail.com>
2017-05-10 10:54:27 +0500

    Basic functionality tested

--- index.js
+++ index.js
@@ -2,81 +2,42 @@ class Queue
 {
 	constructor(queuelengthlimit)
 	{
+		var limit = queuelengthlimit;
+		if (limit==null || Number.isNaN(limit))	limit = Number.MAX_SAFE_INTEGER;
 		Object.defineProperties(
 			this,
 			{
-				_maxLength:{
-					writable:false,
-					enumerable:false,
-					value:queuelengthlimit
-				},
-				_objects:{
-					writeable:true,
-					enumerable:true,
-					value:new Array()
-				}
+				_maxLength:{writable:false,enumerable:false,value:limit},
+				_objects:{writeable:true,enumerable:true,value:new Array()}
 			});
 	}
 }
+
 module.exports = Queue;
 
 Queue.prototype.isEmpty = function() {
-	if (this.size() == 0)
-		return true;
-	else
-		return false;
+	if (this.size() == 0)	return true;
+	else	return false;
 }
-
-Queue.prototype.size = function() {
-	return this._objects.length;
-}
-
-Queue.prototype.memoryOccupied = function(){
-	return 
-}
-
+Queue.prototype.size = function() {return this._objects.length;}
 Queue.prototype.peek = function(position) {
 	if (position)
 	{
-		if (this._objects[position])
-		{
-			return this._objects[position];
-		}
-		else
-		{
-			return null;
-		}
+		if (this._objects[position])	return this._objects[position];
+		else	return null;
 	}
 	else
 	{
-		if (!this.isEmpty())
-		{
-			return this._objects[0];
-		}
-		else
-		{
-			return null;
-		}
+		if (!this.isEmpty())	return this._objects[0];
+		else	return null;
 	}
 }
 Queue.prototype.dequeue = function(){
-	if (this.isEmpty())
-	{
-		return null;
-	}
-	else
-	{
-		return this._objects.shift();
-	}
+	if (this.isEmpty())	return null;
+	else	return this._objects.shift();
 }
 
 Queue.prototype.enqueue = function(obj) {
-	if (obj&&this._maxLength>this.size())
-	{
-		return this._objects.push(obj);
-	}
-	else
-	{
-		return null;
-	}
+	if (obj&&this._maxLength>this.size())	return this._objects.push(obj);
+	else	return null;
 };
