b4180b0e255cbd4855854b88c928aec43f61459f
fahadfarooqmurawat <fahadfarooqmurawat@gmail.com>
2018-08-20 16:21:45 +0500

    TODO:add test cases

--- index.js
+++ index.js
@@ -16,28 +16,29 @@ class Queue
 module.exports = Queue;
 
 Queue.prototype.isEmpty = function() {
-	if (this.size() == 0)	return true;
-	else	return false;
+	if (this.size() === 0) return true;
+	return false;
 }
-Queue.prototype.size = function() {return this._objects.length;}
+Queue.prototype.size = function() {
+	return this._objects.length;
+}
 Queue.prototype.peek = function(position) {
-	if (position)
-	{
-		if (this._objects[position])	return this._objects[position];
-		else	return null;
+	if (position == Number(position)){
+		if (!this._objects[position]) return null;
+		return this._objects[position];
 	}
 	else
 	{
-		if (!this.isEmpty())	return this._objects[0];
-		else	return null;
+		if (this.isEmpty()) return null;
+		return this._objects[0];
 	}
 }
 Queue.prototype.dequeue = function(){
 	if (this.isEmpty())	return null;
-	else	return this._objects.shift();
+	return this._objects.shift();
 }
 
 Queue.prototype.enqueue = function(obj) {
-	if (obj&&this._maxLength>this.size())	return this._objects.push(obj);
-	else	return null;
+	if (obj && this._maxLength>this.size()) return this._objects.push(obj);
+	return null;
 };
